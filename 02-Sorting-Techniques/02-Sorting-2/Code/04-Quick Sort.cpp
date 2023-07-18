d6baec3516007398a8b9112a646db9a4b0d23826
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-07-18 18:17:08 +0530

    Create 04-Quick Sort.cpp

---
+++ 02-Sorting-Techniques/02-Sorting-2/Code/04-Quick Sort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to swap two elements in the array
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function that selects a pivot and partitions the array
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Choose the last element as the pivot
+    int i = low - 1; // Index of the smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        // If the current element is smaller than or equal to the pivot
+        if (arr[j] <= pivot) {
+            i++; // Increment index of the smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1; // Return the partitioning index
+}
+
+// Recursive function to perform QuickSort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // Partition the array and get the partitioning index
+        int pivotIndex = partition(arr, low, high);
+
+        // Recursively sort elements before and after the partition
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {7, 2, 1, 6, 8, 5, 3, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
