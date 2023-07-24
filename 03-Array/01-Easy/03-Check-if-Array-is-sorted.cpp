ffa44f28a1d5e42c9bf0739c9f1edbff58fa5f5d
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-07-24 12:53:06 +0530

    Update 03-Check-if-Array-is-sorted.cpp

--- 03-Array/01-Easy/03-Check-if-Array-is-sorted.cpp
+++ 03-Array/01-Easy/03-Check-if-Array-is-sorted.cpp
@@ -1,7 +1,7 @@
 // Check if array is sorted 
 
 //My soilution 
-
+// TC O(N) , TC O(1)
 class Solution {
 public:
     bool arraySortedOrNot(int arr[], int n) {
@@ -44,3 +44,21 @@ class Solution {
 };
 
 //2
+
+// Approach: Brute Force
+- The idea is pretty simple here, We will start with the element at the 0th index, and will compare it with all of its future elements that are present in the array.
+- If the picked element is smaller than or equal to all of its future values then we will move to the next Index/element until the whole array is traversed.
+- If any of the picked elements is greater than its future elements, Then simply we will return False.
+- If the size of the array is Zero or One i.e ( N = 0 or N = 1 ) or the entire array is traversed successfully then we will simply return True.
+- TC: O(n^2) , SC: O(1)
+    
+bool isSorted(int arr[], int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[i])
+        return false;
+    }
+  }
+
+  return true;
+}
